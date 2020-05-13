0416bf05497a6a58c205d1677a4dc53746554bc9
Leonardo <emailofwork12@gmail.com>
2020-05-14 00:50:51 +0100

    Add files via upload

--- el_bwh/client.lua
+++ el_bwh/client.lua
@@ -17,27 +17,6 @@ function GetIndexedPlayerList()
 	return json.encode(players)
 end
 
-RegisterNUICallback("ban", function(data,cb)
-	if not data.target or not data.reason then return end
-	ESX.TriggerServerCallback("el_bwh:ban",function(success,reason)
-		if success then ESX.ShowNotification("~g~Successfully banned player") else ESX.ShowNotification(reason) end -- dont ask why i did it this way, im a bit retarded
-	end, data.target, data.reason, data.length, data.offline)
-end)
-
-RegisterNUICallback("warn", function(data,cb)
-	if not data.target or not data.message then return end
-	ESX.TriggerServerCallback("el_bwh:warn",function(success)
-		if success then ESX.ShowNotification("~g~Successfully warned player") else ESX.ShowNotification("~r~Something went wrong") end
-	end, data.target, data.message, data.anon)
-end)
-
-RegisterNUICallback("unban", function(data,cb)
-	if not data.id then return end
-	ESX.TriggerServerCallback("el_bwh:unban",function(success)
-		if success then ESX.ShowNotification("~g~Successfully unbanned player") else ESX.ShowNotification("~r~Something went wrong") end
-	end, data.id)
-end)
-
 RegisterNUICallback("getListData", function(data,cb)
 	if not data.list or not data.page then cb(nil); return end
 	ESX.TriggerServerCallback("el_bwh:getListData",function(data)
@@ -47,56 +26,6 @@ end)
 
 RegisterNUICallback("hidecursor", function(data,cb)
 	SetNuiFocus(false, false)
-end)
-
-RegisterNetEvent("el_bwh:gotBanned")
-AddEventHandler("el_bwh:gotBanned",function(rsn)
-	Citizen.CreateThread(function()
-		local scaleform = RequestScaleformMovie("mp_big_message_freemode")
-		while not HasScaleformMovieLoaded(scaleform) do Citizen.Wait(0) end
-		BeginScaleformMovieMethod(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-		PushScaleformMovieMethodParameterString("~r~BANNED")
-		PushScaleformMovieMethodParameterString(rsn)
-		PushScaleformMovieMethodParameterInt(5)
-		EndScaleformMovieMethod()
-		PlaySoundFrontend(-1, "LOSER", "HUD_AWARDS")
-		ClearDrawOrigin()
-		ESX.UI.HUD.SetDisplay(0)
-		while true do
-			Citizen.Wait(0)
-			DisableAllControlActions(0)
-			DisableFrontendThisFrame()
-			local ped = GetPlayerPed(-1)
-			ESX.UI.Menu.CloseAll()
-			SetEntityCoords(ped, 0, 0, 0, 0, 0, 0, false)
-			FreezeEntityPosition(ped, true)
-			DrawRect(0.0,0.0,2.0,2.0,0,0,0,255)
-			DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
-		end
-		SetScaleformMovieAsNoLongerNeeded(scaleform)
-	end)
-end)
-
-RegisterNetEvent("el_bwh:receiveWarn")
-AddEventHandler("el_bwh:receiveWarn",function(sender,message)
-	TriggerEvent("chat:addMessage",{color={255,255,0},multiline=true,args={"BWH","You received a warning"..(sender~="" and " from "..sender or "").."!\n-> "..message}})
-	Citizen.CreateThread(function()
-		local scaleform = RequestScaleformMovie("mp_big_message_freemode")
-		while not HasScaleformMovieLoaded(scaleform) do Citizen.Wait(0) end
-		BeginScaleformMovieMethod(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-		PushScaleformMovieMethodParameterString("~y~WARNING")
-		PushScaleformMovieMethodParameterString(message)
-		PushScaleformMovieMethodParameterInt(5)
-		EndScaleformMovieMethod()
-		PlaySoundFrontend(-1, "LOSER", "HUD_AWARDS")
-		local drawing = true
-		Citizen.SetTimeout(Config.warning_screentime,function() drawing = false end)
-		while drawing do
-			Citizen.Wait(0)
-			DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
-		end
-		SetScaleformMovieAsNoLongerNeeded(scaleform)
-	end)
 end)
 
 --Request Assist--
@@ -135,18 +64,6 @@ AddEventHandler("el_bwh:hideAssistPopup",function(t)
 	last_assist=nil
 end)--END--
 
-RegisterNetEvent("el_bwh:showWindow")
-AddEventHandler("el_bwh:showWindow",function(win)
-	if win=="ban" or win=="warn" then
-		SendNUIMessage({show=true,window=win,players=GetIndexedPlayerList()})
-	elseif win=="banlist" or win=="warnlist" then
-		SendNUIMessage({loading=true,window=win})
-		ESX.TriggerServerCallback(win=="banlist" and "el_bwh:getBanList" or "el_bwh:getWarnList",function(list,pages)
-			SendNUIMessage({show=true,window=win,list=list,pages=pages})
-		end)
-	end
-	SetNuiFocus(true, true)
-end)
 --Assit--
 RegisterCommand("decassist",function(a,b,c)
 	TriggerEvent("el_bwh:hideAssistPopup")
--- el_bwh/server.lua
+++ el_bwh/server.lua
@@ -10,7 +10,6 @@ Citizen.CreateThread(function() -- startup
     
     MySQL.ready(function()
         refreshNameCache()
-        refreshBanCache()
     end)
 
     sendToDiscord("Starting logger...")
@@ -40,100 +39,6 @@ Citizen.CreateThread(function() -- startup
             end
         end
     end, "POST", "resname=el_bwh&ver=1.7")
-
-    ESX.RegisterServerCallback("el_bwh:ban", function(source,cb,target,reason,length,offline)
-        if not target or not reason then return end
-        local xPlayer = ESX.GetPlayerFromId(source)
-        local xTarget = ESX.GetPlayerFromId(target)
-        if not xPlayer or (not xTarget and not offline) then cb(nil); return end
-        if isAdmin(xPlayer) then
-            local success, reason = banPlayer(xPlayer,offline and target or xTarget,reason,length,offline)
-            cb(success, reason)
-        else logUnfairUse(xPlayer); cb(false) end
-    end)
-
-    ESX.RegisterServerCallback("el_bwh:warn",function(source,cb,target,message,anon)
-        if not target or not message then return end
-        local xPlayer = ESX.GetPlayerFromId(source)
-        local xTarget = ESX.GetPlayerFromId(target)
-        if not xPlayer or not xTarget then cb(nil); return end
-        if isAdmin(xPlayer) then
-            warnPlayer(xPlayer,xTarget,message,anon)
-            cb(true)
-        else logUnfairUse(xPlayer); cb(false) end
-    end)
-
-    ESX.RegisterServerCallback("el_bwh:getWarnList",function(source,cb)
-        local xPlayer = ESX.GetPlayerFromId(source)
-        -- local start = GetGameTimer() -- debug
-        if isAdmin(xPlayer) then
-            local warnlist = {}
-            for k,v in ipairs(MySQL.Sync.fetchAll("SELECT * FROM bwh_warnings LIMIT @limit",{["@limit"]=Config.page_element_limit})) do
-                v.receiver_name=namecache[v.receiver]
-                v.sender_name=namecache[v.sender]
-                table.insert(warnlist,v)
-            end
-            cb(json.encode(warnlist),MySQL.Sync.fetchScalar("SELECT CEIL(COUNT(id)/@limit) FROM bwh_warnings",{["@limit"]=Config.page_element_limit}))
-        else logUnfairUse(xPlayer); cb(false) end
-        -- TriggerClientEvent("chat:addMessage",source,{multiline=false,args={"[^4DEBUG^7] ^1BWH",("Warnlist loading time: %sms"):format(GetGameTimer()-start)}}) -- debug
-    end)
-
-    ESX.RegisterServerCallback("el_bwh:getBanList",function(source,cb)
-        local xPlayer = ESX.GetPlayerFromId(source)
-        -- local start = GetGameTimer() -- debug
-        if isAdmin(xPlayer) then
-            local data = MySQL.Sync.fetchAll("SELECT * FROM bwh_bans LIMIT @limit",{["@limit"]=Config.page_element_limit})
-            local banlist = {}
-            for k,v in ipairs(data) do
-                v.receiver_name = namecache[json.decode(v.receiver)[1]]
-                v.sender_name = namecache[v.sender]
-                table.insert(banlist,v)
-            end
-            cb(json.encode(banlist),MySQL.Sync.fetchScalar("SELECT CEIL(COUNT(id)/@limit) FROM bwh_bans",{["@limit"]=Config.page_element_limit}))
-        else logUnfairUse(xPlayer); cb(false) end
-        -- TriggerClientEvent("chat:addMessage",source,{multiline=false,args={"[^4DEBUG^7] ^1BWH",("Banlist loading time: %sms"):format(GetGameTimer()-start)}}) -- debug
-    end)
-
-    ESX.RegisterServerCallback("el_bwh:getListData",function(source,cb,list,page)
-        local xPlayer = ESX.GetPlayerFromId(source)
-        if isAdmin(xPlayer) then
-            if list=="banlist" then
-                local banlist = {}
-                for k,v in ipairs(MySQL.Sync.fetchAll("SELECT * FROM bwh_bans LIMIT @limit OFFSET @offset",{["@limit"]=Config.page_element_limit,["@offset"]=Config.page_element_limit*(page-1)})) do
-                    v.receiver_name = namecache[json.decode(v.receiver)[1]]
-                    v.sender_name = namecache[v.sender]
-                    table.insert(banlist,v)
-                end
-                cb(json.encode(banlist))
-            else
-                local warnlist = {}
-                for k,v in ipairs(MySQL.Sync.fetchAll("SELECT * FROM bwh_warnings LIMIT @limit OFFSET @offset",{["@limit"]=Config.page_element_limit,["@offset"]=Config.page_element_limit*(page-1)})) do
-                    v.sender_name=namecache[v.sender]
-                    v.receiver_name=namecache[v.receiver]
-                    table.insert(warnlist,v)
-                end
-                cb(json.encode(warnlist))
-            end
-        else logUnfairUse(xPlayer); cb(nil) end
-    end)
-
-    ESX.RegisterServerCallback("el_bwh:unban",function(source,cb,id)
-        local xPlayer = ESX.GetPlayerFromId(source)
-        if isAdmin(xPlayer) then
-            MySQL.Async.execute("UPDATE bwh_bans SET unbanned=1 WHERE id=@id",{["@id"]=id},function(rc)
-                local bannedidentifier = "N/A"
-                for k,v in ipairs(bancache) do
-                    if v.id==id then
-                        bannedidentifier = v.receiver[1]
-                        bancache[k].unbanned = true
-                        break
-                    end
-                end
-                logAdmin(("Admin ^1%s^7 unbanned ^1%s^7 (%s)"):format(xPlayer.getName(),(bannedidentifier~="N/A" and namecache[bannedidentifier]) and namecache[bannedidentifier] or "N/A",bannedidentifier))
-                cb(rc>0)
-            end)
-        else logUnfairUse(xPlayer); cb(false) end
-    end)
 end)
 
 AddEventHandler("playerConnecting",function(name, setKick, def)
@@ -161,7 +66,6 @@ AddEventHandler("playerConnecting",function(name, setKick, def)
     end
 end)
 
---assist--
 AddEventHandler("playerDropped",function(reason)
     if open_assists[source] then open_assists[source]=nil end
     for k,v in ipairs(active_assists) do
@@ -178,19 +82,10 @@ AddEventHandler("playerDropped",function(reason)
     end
 end)
 
---END--
-
 function refreshNameCache()
     namecache={}
     for k,v in ipairs(MySQL.Sync.fetchAll("SELECT steam,name FROM bwh_identifiers")) do
         namecache[v.steam]=v.name
-    end
-end
-
-function refreshBanCache()
-    bancache={}
-    for k,v in ipairs(MySQL.Sync.fetchAll("SELECT id,receiver,sender,reason,UNIX_TIMESTAMP(length) AS length,unbanned FROM bwh_bans")) do
-        table.insert(bancache,{id=v.id,sender=v.sender,sender_name=namecache[v.sender]~=nil and namecache[v.sender] or "N/A",receiver=json.decode(v.receiver),reason=v.reason,length=v.length,unbanned=v.unbanned==1})
     end
 end
 
@@ -199,7 +94,7 @@ function sendToDiscord(msg)
         PerformHttpRequest(Config.discord_webhook, function(a,b,c)end, "POST", json.encode({embeds={{title="BWH Action Log",description=msg:gsub("%^%d",""),color=65280,}}}), {["Content-Type"]="application/json"})
     end
 end
---LOGAdmin--
+
 function logAdmin(msg)
     for k,v in ipairs(ESX.GetPlayers()) do
         if isAdmin(ESX.GetPlayerFromId(v)) then
@@ -208,7 +103,7 @@ function logAdmin(msg)
         end
     end
 end
---END--
+
 
 function isBanned(identifiers)
     for _,ban in ipairs(bancache) do
@@ -240,65 +135,6 @@ function execOnAdmins(func)
     end
     return ac
 end
-
-function logUnfairUse(xPlayer)
-    if not xPlayer then return end
-    print(("[^1"..GetCurrentResourceName().."^7] Player %s (%s) tried to use an admin feature"):format(xPlayer.getName(),xPlayer.identifier))
-    logAdmin(("Player %s (%s) tried to use an admin feature"):format(xPlayer.getName(),xPlayer.identifier))
-end
-
-function banPlayer(xPlayer,xTarget,reason,length,offline)
-    local targetidentifiers,offlinename,timestring,data = {},nil,nil,nil
-    if offline then
-        data = MySQL.Sync.fetchAll("SELECT * FROM bwh_identifiers WHERE steam=@identifier",{["@identifier"]=xTarget})
-        if #data<1 then
-            return false, "~r~Identifier is not in identifiers database!"
-        end
-        offlinename = data[1].name
-        for k,v in pairs(data[1]) do
-            if k~="name" then table.insert(targetidentifiers,v) end
-        end
-    else
-        targetidentifiers = GetPlayerIdentifiers(xTarget.source)
-    end
-    if length=="" then length = nil end
-    MySQL.Async.execute("INSERT INTO bwh_bans(id,receiver,sender,length,reason) VALUES(NULL,@receiver,@sender,@length,@reason)",{["@receiver"]=json.encode(targetidentifiers),["@sender"]=xPlayer.identifier,["@length"]=length,["@reason"]=reason},function(_)
-        local banid = MySQL.Sync.fetchScalar("SELECT MAX(id) FROM bwh_bans")
-        logAdmin(("Player ^1%s^7 (%s) got banned by ^1%s^7, expiration: %s, reason: '%s'"..(offline and " (OFFLINE BAN)" or "")):format(offline and offlinename or xTarget.getName(),offline and data[1].steam or xTarget.identifier,xPlayer.getName(),length~=nil and length or "PERMANENT",reason))
-        if length~=nil then
-            timestring=length
-            local year,month,day,hour,minute = string.match(length,"(%d+)/(%d+)/(%d+) (%d+):(%d+)")
-            length = os.time({year=year,month=month,day=day,hour=hour,min=minute})
-        end
-        table.insert(bancache,{id=banid==nil and "1" or banid,sender=xPlayer.identifier,reason=reason,sender_name=xPlayer.getName(),receiver=targetidentifiers,length=length})
-        if offline then xTarget = ESX.GetPlayerFromIdentifier(xTarget) end -- just in case the player is on the server, you never know
-        if xTarget then
-            TriggerClientEvent("el_bwh:gotBanned",xTarget.source, reason)
-            Citizen.SetTimeout(5000, function()
-                DropPlayer(xTarget.source,Config.banformat:format(reason,length~=nil and timestring or "PERMANENT",xPlayer.getName(),banid==nil and "1" or banid))
-            end)
-        else return false, "~r~Unknown error (MySQL?)" end
-        return true, ""
-    end)
-end
-
-function warnPlayer(xPlayer,xTarget,message,anon)
-    MySQL.Async.execute("INSERT INTO bwh_warnings(id,receiver,sender,message) VALUES(NULL,@receiver,@sender,@message)",{["@receiver"]=xTarget.identifier,["@sender"]=xPlayer.identifier,["@message"]=message})
-    TriggerClientEvent("el_bwh:receiveWarn",xTarget.source,anon and "" or xPlayer.getName(),message)
-    logAdmin(("Admin ^1%s^7 warned ^1%s^7 (%s), Message: '%s'"):format(xPlayer.getName(),xTarget.getName(),xTarget.identifier,message))
-end
-
-AddEventHandler("el_bwh:ban",function(sender,target,reason,length,offline)
-    if source=="" then -- if it's from server only
-        banPlayer(sender,target,reason,length,offline)
-    end
-end)
-
-AddEventHandler("el_bwh:warn",function(sender,target,message,anon)
-    if source=="" then -- if it's from server only
-        warnPlayer(sender,target,message,anon)
-    end
-end)
 
 --Assist--
 TriggerEvent('es:addCommand', 'assist', function(source, args, user)
@@ -349,18 +185,15 @@ TriggerEvent('es:addCommand', 'finassist', function(source, args, user)
     else
         TriggerClientEvent("chat:addMessage",source,{color={255,0,0},multiline=false,args={"BWH","Não tens permissões para este comando!"}})
     end
-end)--END--
+end)
 
 TriggerEvent('es:addCommand', 'bwh', function(source, args, user)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if isAdmin(xPlayer) then --Desnecessario--
-        if args[1]=="ban" or args[1]=="warn" or args[1]=="warnlist" or args[1]=="banlist" then 
-            TriggerClientEvent("el_bwh:showWindow",source,args[1])
-        elseif args[1]=="refresh" then
-            TriggerClientEvent("chat:addMessage",source,{color={0,255,0},multiline=false,args={"BWH","Refreshing ban & name cache..."}})
+    if isAdmin(xPlayer) then
+        if args[1]=="refresh" then
+            TriggerClientEvent("chat:addMessage",source,{color={0,255,0},multiline=false,args={"BWH","Refreshing name cache..."}})
             refreshNameCache()
-            refreshBanCache() --Desnecessario
-        elseif args[1]=="assists" then --Assist--
+        elseif args[1]=="assists" then
             local openassistsmsg,activeassistsmsg = "",""
             for k,v in pairs(open_assists) do
                 openassistsmsg=openassistsmsg.."^5ID "..k.." ("..GetPlayerName(k)..")^7 - "..v.."\n"
@@ -414,18 +247,4 @@ AddEventHandler("el_bwh:acceptAssistKey",function(target)
     if not target then return end
     local _source = source
     acceptAssist(ESX.GetPlayerFromId(_source),target)
-end)--END--
-
-if Config.enable_ban_json or Config.enable_warning_json then
-    SetHttpHandler(function(req,res)
-        if req.path=="/bans.json" and Config.enable_ban_json then
-            MySQL.Async.fetchAll("SELECT * FROM bwh_bans",{},function(data)
-                res.send(json.encode(data))
-            end)
-        elseif req.path=="/warnings.json" and Config.enable_warning_json then
-            MySQL.Async.fetchAll("SELECT * FROM bwh_warnings",{},function(data)
-                res.send(json.encode(data))
-            end)
-        end
-    end)
-end
+end)
